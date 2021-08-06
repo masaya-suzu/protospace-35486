5ca5f603fc85bc39de4dccb73194515a28e1bf41
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-08-06 19:51:51 +0900

    再提出３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -31,7 +31,7 @@ class PrototypesController < ApplicationController
   def update
     if @prototype.update(prototype_params)
       flash[:success] = "Profile updated"
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
